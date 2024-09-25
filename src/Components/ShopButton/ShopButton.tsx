207fcb52d7e66e5d72a094b22e4474d3e7d6b16f
aliyevshukur <shukuralyv@gmail.com>
2024-09-25 22:37:12 +0400

    Make shop button responsive

--- src/Components/ShopButton/ShopButton.tsx
+++ src/Components/ShopButton/ShopButton.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { FaArrowRight } from "react-icons/fa";
-import "./ShopButton.scss";
 import { scrollToProducts } from '../../Utils/ScrollToElement';
+import "./ShopButton.scss";
 
 
 export default function ShopButton() {
     return (
-        <div className='shop-button'>
+        <div className='shop-button' onClick={(e) => scrollToProducts(e)}>
             Shop now
-            <div className="arrow-icon" onClick={(e) => scrollToProducts(e)}>
+            <div className="arrow-icon" >
                 <FaArrowRight color={"white"} />
             </div>
         </div>
