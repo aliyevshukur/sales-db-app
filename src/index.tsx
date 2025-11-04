aa41d9f7726d8b8bfa82f4dc95e2d8c306143faa
aliyevshukur <shukuralyv@gmail.com>
2025-11-04 14:42:03 +0400

    Migrated project to Vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import "./index.css"
-import reportWebVitals from "./reportWebVitals"
-import About from "./Routes/About/About"
-import App from "./Routes/App"
-import ErrorPage from "./Routes/Error/ErrorPage"
-import Home from "./Routes/Home/Home"
-import Product from "./Routes/Product/Product"
-import RecieptPage from "./Routes/RecieptPage/RecieptPage"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import About from "./Routes/About/About";
+import App from "./Routes/App";
+import ErrorPage from "./Routes/Error/ErrorPage";
+import Home from "./Routes/Home/Home";
+import Product from "./Routes/Product/Product";
+import RecieptPage from "./Routes/RecieptPage/RecieptPage";
 
 const router = createBrowserRouter([
   {
@@ -35,16 +34,13 @@ const router = createBrowserRouter([
       },
     ],
   },
-])
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+);
