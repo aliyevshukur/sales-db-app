81c761cd2cd58c2724b40dcb1f473d576ae2db22
aliyevshukur <shukuralyv@gmail.com>
2025-11-04 15:08:15 +0400

    Remove redundant files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import "./index.scss";
 import About from "./Routes/About/About";
 import App from "./Routes/App";
 import ErrorPage from "./Routes/Error/ErrorPage";
